df0bf8eaabe57d7a57c93beaec6047a775a7dafd
sijibomi <gbemilesanmi@gmail.com>
2023-06-02 01:57:20 +0100

    feat:

--- src/components/Methods.tsx
+++ src/components/Methods.tsx
@@ -51,7 +51,6 @@ export default function Methods() {
           <Card />
 
         </Slider>
-        
       </section>
     </main>
   )
--- src/minicomponents/Card.tsx
+++ src/minicomponents/Card.tsx
@@ -1,7 +1,7 @@
 
 export default function Card() {
   return (
-    <div className="bg-white drop-shadow-md rounded-xl overflow-hidden mx-7 m-4">
+    <div className="bg-white drop-shadow-md rounded-xl overflow-hidden mx-5">
         <img src="/wavy.jpg" className="h-40 w-full object-cover object-center" />
         <div className="p-2 md:p-3 border-b">
             <h1 className="py-1 md:py-1 text-xs md:text-sm font-rubik text-black font-extralight">Place Holder category</h1>
